e72ce0e423bc3d8958279c074408401d3a73f6aa
Albina315 <gabdelkhakovaa@mail.ru>
2025-03-04 17:46:43 +0300

    Добавьте файлы проекта.

---
+++ DVD/Pages/PageQr.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using QRCoder;
+
+namespace DVD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageQr.xaml
+    /// </summary>
+    public partial class PageQr : Page
+    {
+        public PageQr()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateQrBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string soucer_x1 = "https://static10.tgstat.ru/channels/_0/22/223074ca5ddcf3796c3a812ad14e5bda.jpg";
+            QRCoder.QRCodeGenerator qr = new QRCoder.QRCodeGenerator();
+            QRCoder.QRCodeData data = qr.CreateQrCode(soucer_x1, QRCoder.QRCodeGenerator.ECCLevel.L);
+            QRCoder.QRCode code = new QRCoder.QRCode(data);
+            Bitmap bitmap = code.GetGraphic(100);
+            using (MemoryStream memory = new MemoryStream()) 
+            {
+                bitmap.Save(memory,System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                ImageQr.Source=bitmapImage;
+              
+            }
+
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AutorizationPage());
+        }
+    }
+}
---
+++ DVD/Pages/RegisrtationPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DVD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisrtationPage.xaml
+    /// </summary>
+    public partial class RegisrtationPage : Page
+    {
+        public RegisrtationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegesrtRegbtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AutorizationPage());
+        }
+    }
+}
